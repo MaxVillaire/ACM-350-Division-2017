9ff9ac07bfcfbc7a143657e732d2955c841c82d1
MaxVillaire <villaire@email.sc.edu>
2017-11-16 13:38:50 -0500

    Update PrismaticPancakes.py

--- PrismaticPancakes.py
+++ PrismaticPancakes.py
@@ -3,5 +3,4 @@
 #https://www.hackerrank.com/contests/usc-acm-fall-2017-350-division/challenges/prismatic-pancakes
 #Written By: Maxwell Villaire
 #This was a trick question, all answers are "2" except for n=1
-if raw_input() == "1": print "1"
-else: print "2"
+print int(raw_input() != "1")+1
